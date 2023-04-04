aba8c726d68ba9573397e61ba558d1dfe747c59a
Matt Hays <mehays@protonmail.com>
2023-04-03 23:35:14 -0400

    Migrate local state to S3 using AWS SSO

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+from os import name
 from constructs import Construct
 from cdktf import App, TerraformStack, S3Backend
 from imports.aws.provider import AwsProvider
-import os
+from imports.aws.s3_bucket import S3Bucket
+
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
@@ -10,24 +12,28 @@ class MyStack(TerraformStack):
 
         provider = AwsProvider(
             self,
-            "default_provider",
-            region="us-east-1"
+            "AWS",
+            region="us-east-1",
+            profile="mehays-deployer"
         )
-
+        
         S3Backend(
             self,
             bucket="mehays-terraform",
             key="main/tfstate",
+            encrypt=True,
             region=provider.region,
-            access_key=os.getenv("ACCESS_KEY"),
-            secret_key=os.getenv("SECRET_KEY")
-
-
-
-
+            profile="mehays-deployer",
         )
 
+        site_bucket = S3Bucket(
+            self,
+            "site",
+            bucket="matthays.dev"
+        )
 
+        #TODO: Decide how to build and publish the site
+        #TODO: Config parser with "envs" deploying to different buckets
 app = App()
 MyStack(app, "matt-hays-site-infra")
 
